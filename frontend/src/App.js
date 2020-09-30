0be54cc01aa3fc4c89c7f91745c4ddb646e052d5
Mario Lazzari <mario.lazzari@gmail.com>
2020-09-30 21:28:49 +0200

    Done lesson 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      Welcome
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
