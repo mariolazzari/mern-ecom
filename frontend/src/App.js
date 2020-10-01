259fcb7295c4178def1154244d73ccc77f2bafde
Mario Lazzari <mario.lazzari@gmail.com>
2020-10-01 22:52:05 +0200

    DOne lesson 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
